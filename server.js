cef53b7e7cc309fff800808ad99fa44a4f8b18de
jan <pascalvdbosch@hotmail.com>
2020-10-21 22:27:16 +0000

    works!

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 (async () => {
-    const {performance} = require('perf_hooks');
     const express = require('express');
     const cookieParser = require('cookie-parser');
 
@@ -9,22 +8,22 @@
     // databaseuser
     // uPmQs9eKrp3BBmUa
 
-
-    const play = fs.readFileSync('app/play.html', 'utf8');
-    const viewer  = fs.readFileSync('app/viewer.html', 'utf8');
-    const counter = fs.readFileSync('app/counter.html', 'utf8');
-    const adminlogin = fs.readFileSync('app/adminlogin.html', 'utf8');
-    const nextquestioncomposer = fs.readFileSync('app/nextquestioncomposer.html', 'utf8');
+    const nextquestioncomposer = ""; // fs.readFileSync('app/nextquestioncomposer.html', 'utf8');
 
     const secret = fs.readFileSync('secret', 'utf8');
 
     const app = express();
+    var http = require('http').createServer(app);
     app.use(cookieParser());
+    var io = require('socket.io')(http);
+    
+    var kind = "";
 
-    app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*"); 
-        next();
-      });
+    // kind = "": no game
+    // kind = "janee": ja/nee game
+    // kind = "textinput": willekeurige tekst
+    // kind = "abc": a b c
+    // kind = "abcd": a b c d
 
     var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb+srv://databaseuser:uPmQs9eKrp3BBmUa@mycluster.jcyct.azure.mongodb.net/mydb?retryWrites=true&w=majority";
@@ -32,99 +31,79 @@
     var dbo = db.db("mydb");
     var results = dbo.collection("results");
     var games = dbo.collection("games");
-    var users = dbo.collection("users");
+    
+    io.on('connection', (socket) => {
+        if (kind != "") socket.emit("start", kind);
+        socket.on('send', (studentnummer, data) => {
+            let i = {
+                time: (new Date()).getTime(), 
+                ip: socket.request.connection.remoteAddress, 
+                studentnummer: studentnummer, 
+                data: data};
+            results.insertOne(i).then(() => res.send("succes"));
+            console.log(i);
+        });
+        socket.on('end', (s) => {
+            if (secret != s) return;
+            games.find().sort({start: 1}).limit(1).toArray().then((a) => {
+                let lastgame = a[0];
+                if ("end" in Object.keys(lastgame))
+                    return;
+                lastgame.end = (new Date()).getTime();
+                games.updateOne({ "_id" : lastgame._id }, lastgame).then(() => io.emit('end'));
+            });
+        });
+        socket.on('start', (s, k) => {
+            if (secret == s) {
+                kind = k;
+                games.insertOne({
+                    start: (new Date()).getTime(), 
+                    kind: kind
+                }).then(() => {
+                    io.emit('end');
+                    io.emit('start', kind); 
+                    console.log("started");
+                }); 
+            }
+        });
+    });
 
-    var game = {
-        started: 0, 
-        maxtime: 0,
-        kind: -1
-    };
+    app.get('/', (req, res) => res.sendFile("index.html", { root: '.' })); 
+    app.get('/play', (req, res) => res.sendFile("play.html", { root: '.' })); 
+    app.get('/admin', (req, res) => res.sendFile("admin.html", { root: '.' }));
 
-    function logme(req, f) {
-        let t1 = performance.now();
-        f();
-        let t2 = performance.now();
-        console.log("GET " + req.url + " FROM " + req.connection.remoteAddress);
-        console.log(t2 - t1);
-    }
-
-    function get(url, func) {
-        app.get(url, async (req, res) => logme(req, async () => func(req, res)));
-    }
-
-    get('/', (req, res) => res.send(play)); 
-
-    function addmetadata(d, req) {
-        return Object.assign({}, d, {time: (new Date()).getTime(), ip: req.connection.remoteAddress});
-    }
-
-    app.post('/data', async (req, res) => logme(req, async () => {
-        let r = users.find({name: req.query.name}); 
-        const aaa = await r.limit(1).toArray();
-        if ((await r.count()) == 0)
-            await users.insertOne(addmetadata({name: req.query.name, client_info: req.query.client_info}, req));
-        else if ((await r.limit(1).toArray()).client_info != req.query.client_info) {
-            res.send("fail");
-            return;
-        }
-        results.insertOne(addmetadata({name: req.query.naam, data: req.body}, req)).then(() => res.send("succes"));
-    }));
-
-    get('/game', async (_, res) => res.send(game));
-
-    get('/viewer', 
+    app.get('/viewer', 
         async (req, res) => {
-            let lastgame = (await games.find().sort({started: 1}).limit(1).toArray())[0];
+            let lastgame = (await games.find().sort({start: 1}).limit(1).toArray())[0];
             let datas = await results.aggregate([
-                {$match: {"time": {$gt: lastgame.started}}},
+                {$match: {"time": {$gt: lastgame.start}}},
                 {$sort: {"time": 1}},
-                {$group: {"_id": "$name", data: {$last: "$data"}}},
+                {$group: {"_id": "$studentnummer", data: {$last: "$data"}}},
                 {$project: {data: {$split: ["$data", ","]}}},
                 {$unwind: "$data"},
                 {$project: {data : {$trim: {input: "$data"}}}},
-                {$group: {_id:"$data", aantal: {$sum: 1}}},
-                {$sort: {"aantal": 1}},
+                {$group: {_id: "$data", aantal: {$sum: 1}}},
+                {$sort: {"aantal": -1}},
             ]).toArray();
             console.log(datas);
-            let h = Object.keys(datas[0]).filter((key) => !key.startsWith("_"));
-            res.send("<table>" + ("<th>" + h.join("</th><th>") + "</th>") + ("<tr>" + datas.map((d) => "<td>" + h.map((hh) => d[hh]).join("</td><td>") + "</td>").join("</tr><tr>") + "</tr>") + "</table>");
+            if (datas == null || datas.length == 0)
+                res.send("empty!");
+            else {
+                let h = Object.keys(datas[0]); //.filter((key) => !key.startsWith("_"));
+                res.send("<table>" + ("<th>" + h.join("</th><th>") + "</th>") + ("<tr>" + datas.map((d) => "<td>" + h.map((hh) => d[hh]).join("</td><td>") + "</td>").join("</tr><tr>") + "</tr>") + "</table>");
+            }
     });
 
     function access(req) {
         return req.cookies['secret'] == secret;
     }
-    get('/nextquestioncomposer', async (req, res) => res.send(access(req) ? nextquestioncomposer : "wrong secret"));
+    app.get('/nextquestioncomposer', async (req, res) => res.send(access(req) ? nextquestioncomposer : "wrong secret"));
 
-    async function nextquestion(req) {
-        if (access(req)) {
-            game = {
-                started: (new Date()).getTime(), 
-                maxtime: req.query.maxtime,
-                kind: req.query.kind };
-            await games.insertOne(game); 
-            return true;
-        }
-        return false;
-    }
 
-    get('/nextquestion', 
-        async (req, res) => {
-            if (nextquestion(req))
-                res.send("succes");
-            else
-                res.send("wrong secret");
+    io.on('connection', (socket) => {
+    console.log('a user connected');
     });
 
-    get('/nextquestionviewer', 
-        async (req, res) => {
-            if (nextquestion(req))
-                res.send(counter.replace("##startval##", req.query.maxtime / 1000));
-            else
-                res.send("wrong secret");
-    });
-    
-    get('/adminlogin', (req, res) => res.send(adminlogin));
-
-    app.listen(process.env.PORT);
+    http.listen(process.env.PORT); // 8000
     console.log("Listening");
 })();
