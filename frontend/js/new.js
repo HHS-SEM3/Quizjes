fd4da7f6364a20b9a343e1632f96d0b9d3081c9a
jan <pascalvdbosch@hotmail.com>
2020-11-02 08:14:05 +0000

    fixed arguments

--- frontend/js/new.js
+++ frontend/js/new.js
@@ -4,11 +4,23 @@ function getCookie(name) {
 }
 
 if (getCookie("secret") == null) window.location.href = "adminlogin.html";
+else {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
 
+    var socket = io();
+    document.getElementById("time-input").value = urlParams.get("time") || 40;
+    const kind = urlParams.get("kind") || "abcd";
+    if (kind.startsWith("codegolfjs")) {
+        document.getElementById('bestand-input').value = kind.substr("codegolfjs-".length);
+        kind = "codegolfjs";
+    }
+    document.getElementById('select-question-type').value = kind;
+}
 function f(e) {
     e.preventDefault();
     let kind = document.getElementById('select-question-type').value;
     if (kind == 'codegolfjs')
         kind += '-' + document.getElementById('bestand-input').value;
-        window.location.href = 'timer.html?kind=' + kind + '&time=' + document.getElementById("time-input").value;
+    window.location.href = 'timer.html?kind=' + kind + '&time=' + document.getElementById("time-input").value;
 }
